9e71d0bc6647963a0830ce794088286881623186
Noé Plantier <plantiernoe50@gmail.com>
2025-02-24 19:21:01 +0100

    New ribbons component added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,13 +8,31 @@ export function Hero() {
   return (
     <div className="relative min-h-screen">
     <div className="second-container text-center my-2 p-6" id="second-container">
-      <h1 className="text-gray-100 font-semibold text-9xl mt-4 mb-4" style={{fontStyle:'italic', fontFamily:'DM Serif Display'}}> 
-        Cozette Fleurisson
+      <h1 className="text-gray-100 font-semibold text-9xl mt-4 mb-4" style={{fontStyle:'italic'}}>
+        {["Cosette", "Fleurisson"].map((word, wordIndex) => (
+          <span key={wordIndex}>
+            {Array.from(word).map((letter, letterIndex) => (
+              <span key={letterIndex} className="hover-effect">
+                {letter}
+              </span>
+            ))}
+            {wordIndex < 1 && " "}
+          </span>
+        ))}
       </h1>
       
-      <h2 className="text-gray-100 font-semibold text-5xl mt-4 mb-4" style={{fontStyle:'italic', fontFamily:'DM Serif Display'}}> 
-        Community Manager & Social Media Expert
-        </h2>
+      <h2 className="text-gray-100 font-semibold text-5xl mt-4 mb-4" style={{fontStyle:'italic', fontFamily:'DM Serif Display'}}>
+        {["Community", "Manager", "&", "Social", "Media", "Expert"].map((word, wordIndex) => (
+          <span key={wordIndex}>
+            {Array.from(word).map((letter, letterIndex) => (
+              <span key={letterIndex} className="hover-effect-small">
+                {letter}
+              </span>
+            ))}
+            {wordIndex < 5 && " "}
+          </span>
+        ))}
+      </h2>
 
         <Image src="/images/cosette.png" alt="Cozette Fleurisson" width={500} height={500} quality={100} className="hero-image rounded-full "/>
         </div>
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Modal, Button, Form } from 'react-bootstrap';
 import '../../index.css';
@@ -14,6 +14,27 @@ const MainTitle = () => {
   const handleCloseSignupModal = () => setShowSignupModal(false);
 
 
+
+  useEffect(() => {
+    const videoElement = document.querySelector('video');
+    if (videoElement) {
+      videoElement.playbackRate = 0.8;
+      
+      const handleTimeUpdate = () => {
+        const timeLeft = videoElement.duration - videoElement.currentTime;
+        if (timeLeft < 0.1) {
+          videoElement.currentTime = 0;
+        }
+      };
+
+      videoElement.addEventListener('timeupdate', handleTimeUpdate);
+      
+      return () => {
+        videoElement.removeEventListener('timeupdate', handleTimeUpdate);
+      };
+    }
+  }, []);
+
   return (
     <div className="relative min-h-screen">
       {/* Vidéo de fond */}
@@ -24,7 +45,7 @@ const MainTitle = () => {
         className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
       >
         <source
-          src="/videos/horloge.mp4" // Chemin vers votre vidéo locale
+          src="/videos/horloge.mp4"
           type="video/mp4"
         />
         Your browser does not support the video tag.
@@ -33,11 +54,29 @@ const MainTitle = () => {
       {/* Contenu principal */}
       <div className="flex justify-center items-center min-h-screen">
         <div className="text-center my-2 p-6" id="main-container">
-          <h1 className="text-white text-9xl font-bold" id="main-title" >
-            Mots de maître
+        <h1 className="text-white text-9xl font-bold" id="main-title">
+            {["Mots", "de", "maître"].map((word, wordIndex) => (
+              <span key={wordIndex}>
+                {Array.from(word).map((letter, letterIndex) => (
+                  <span key={letterIndex} className="hover-effect">
+                    {letter}
+                  </span>
+                ))}
+                {wordIndex < 2 && " "}
+              </span>
+            ))}
           </h1>
           <h2 className="text-white font-semibold text-5xl mt-4">
-            Community Management & Social Media Marketing
+            {["Community", "Management", "&", "Social", "Media", "Marketing"].map((word, wordIndex) => (
+              <span key={wordIndex}>
+                {Array.from(word).map((letter, letterIndex) => (
+                  <span key={letterIndex} className="hover-effect-small">
+                    {letter}
+                  </span>
+                ))}
+                {wordIndex < 5 && " "}
+              </span>
+            ))}
           </h2>
           <div className="mt-8">
 
